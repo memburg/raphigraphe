d1251cd5de2e7672f305d27bd25ac48341e7d017
Ramb Memburg <ramb.memburg@gmail.com>
2025-05-01 13:29:58 -0400

    [feat]: remove go

--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/memburg/raphigraphe
-
-go 1.24.2
--- raphigraphe.go
+++
@@ -1,7 +0,0 @@
-package raphigraphe
-
-type Raphigraphe struct{}
-
-func (r *Raphigraphe) TestFunction() string {
-	return "Raphigraphe test function"
-}
