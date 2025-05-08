0ecd5b1eada8123aed36f7edda6014faf9487839
Ramb Memburg <ramb.memburg@gmail.com>
2025-05-08 13:21:47 -0400

    [feat]: convert into braille ascii art

--- raphigraphe/src/main/java/io/github/Raphigraphe.java
+++ raphigraphe/src/main/java/io/github/Raphigraphe.java
@@ -2,6 +2,8 @@ package io.github;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Callable;
 
 import imajine.Pixel;
@@ -69,20 +71,101 @@ public class Raphigraphe implements Callable<Integer> {
         return resizedImage;
     }
 
+    public static Imajine extractSubimage(Imajine image, int x, int y) {
+        int width = Math.min(2, image.getWidth() - x);
+        int height = Math.min(4, image.getHeight() - y);
+        return crop(image, x, y, width, height);
+    }
+
+    public static String getAsciiCharacter(Imajine subimage) throws IOException {
+        Pixel[][] subimagePixels = new Pixel[2][4];
+
+        for (int x = 0; x < 2; x++) {
+            for (int y = 0; y < 4; y++) {
+                subimagePixels[x][y] = subimage.getPixel(x, y);
+            }
+        }
+
+        String closestMatch = "";
+
+        File[] brailleImages = BRAILLE_IMAGES_DIR.listFiles();
+
+        if (brailleImages == null) {
+            throw new IOException("Braille images directory is empty or not found.");
+        }
+
+        for (File asciiImage : brailleImages) {
+            if (asciiImage.getName().equals(".DS_Store")) {
+                continue;
+            }
+
+            Imajine braille = new Imajine(asciiImage.getAbsolutePath());
+
+            int matches = 0;
+
+            for (int x = 0; x < 2; x++) {
+                for (int y = 0; y < 4; y++) {
+                    Pixel p1 = braille.getPixel(x, y);
+                    Pixel p2 = subimagePixels[x][y];
+
+                    if (p1.toString().equals(p2.toString())) {
+                        matches++;
+                    }
+                }
+            }
+
+            if (matches == 8) {
+                return BRAILLE_CHARACTERS.charAt(Integer.parseInt(asciiImage.getName().substring(8, 11))) + "";
+            }
+
+            if (matches == 7) {
+                closestMatch = BRAILLE_CHARACTERS.charAt(Integer.parseInt(asciiImage.getName().substring(8, 11))) + "";
+            }
+        }
+
+        return closestMatch;
+    }
+
+    public static List<List<String>> getAsciiImage(Imajine image) throws IOException {
+        int width = image.getWidth();
+        int height = image.getHeight();
+        List<List<String>> asciiImage = new ArrayList<>();
+
+        for (int y = 0; y < height; y += 4) {
+            List<String> asciiRow = new ArrayList<>();
+
+            for (int x = 0; x < width; x += 2) {
+                Imajine subimage = extractSubimage(image, x, y);
+                asciiRow.add(getAsciiCharacter(subimage));
+            }
+
+            asciiImage.add(asciiRow);
+        }
+
+        return asciiImage;
+    }
+
+    public static void printAsciiImage(List<List<String>> asciiImage) {
+        for (List<String> row : asciiImage) {
+            for (String character : row) {
+                System.out.print(character);
+            }
+            System.out.println();
+        }
+    }
+
     @Override
     public Integer call() throws Exception {
-        final String SOURCE_IMAGE = ROOT_DIR + "/raphigraphe/src/main/resources/test-images/el-gatito.png";
+        final String SOURCE_IMAGE = ROOT_DIR + "/raphigraphe/src/main/resources/test-images/mr-incredible.png";
 
         Imajine imajine = new Imajine(SOURCE_IMAGE);
         Adjustments.threshold(imajine, threshold);
         Imajine cropped = centerCrop(imajine);
-        Imajine resized = resize(imajine, 50, 52);
+        Imajine resized = resize(cropped, 50, 52);
 
-        System.out.println(imajine);
-
-        imajine.save(ROOT_DIR + "/raphigraphe/src/main/resources/output-results/el-gatito-thresholded.png");
-        cropped.save(ROOT_DIR + "/raphigraphe/src/main/resources/output-results/el-gatito-cropped.png");
-        resized.save(ROOT_DIR + "/raphigraphe/src/main/resources/output-results/el-gatito-resized.png");
+        List<List<String>> asciiImage = getAsciiImage(resized);
+        printAsciiImage(asciiImage);
+        System.out.println("ASCII art generated successfully!");
 
         return 0;
     }
